f2b344ed0ed7e8df440a6c7c17110cf003aca170
NZBBR <natanzimmer@hotmail.com>
2020-05-19 20:08:34 -0300

    Profile screen

--- doacao-legal/src/pages/profile/index.js
+++ doacao-legal/src/pages/profile/index.js
@@ -1,13 +1,115 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, ScrollView, AsyncStorage, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import api from '../../service/api';
 
 import styles from './style'
 
 function Profile(){
+    const navigation = useNavigation();
+
+    const [user, setUser] = useState({})
+
+    function signOut() {
+        navigation.goBack();
+    }
+
+    async function getUserData() {
+        if(user) return;
+        
+        const user_Id = await AsyncStorage.getItem('id');
+
+        const resp = await api.get('user', {
+            headers: {
+                Authorization: user_Id
+            }
+        });
+        const user = JSON.parse(resp.request.response);
+
+        setUser(user);
+    }
+
+    useEffect(() => {
+        getUserData();
+    }, [])
+
+
     return (
-        <View style={styles.container}>
-            <Text>Your Profile</Text>
-        </View>
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+        >
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.title}>Perfil do Usuário</Text>
+                </View>
+
+                <View>
+                    <View style={styles.data}>
+                        <TouchableOpacity
+                            style={styles.edit}
+                        >
+                            <Icon name="edit" size={20} color="green"/>
+                        </TouchableOpacity>
+
+                        <Text style={styles.userProperty}>Nome: </Text>
+                        <Text style={styles.userValue}>{user.name}</Text>
+
+                        <Text style={styles.userProperty}>Telefone: </Text>
+                        <Text style={styles.userValue}>{user.telefone}</Text>
+
+                        <Text style={styles.userProperty}>Email: </Text>
+                        <Text style={styles.userValue}>{user.email}</Text>
+                    </View>
+
+                    <View style={styles.data}>
+                        <View style={styles.address}>
+                            <View>
+                                <Text style={styles.userProperty}>Cidade: </Text>
+                                <Text style={styles.userValue}>{user.cidade}</Text>
+                            </View>
+                            <View style={styles.rigthData}>
+                                <Text style={styles.userProperty}>CEP: </Text>
+                                <Text style={styles.userValue}>{user.cep}</Text>
+                            </View>
+                        </View>
+
+                        <View style={styles.address}>
+                            <View>
+                                <Text style={styles.userProperty}>Bairro: </Text>
+                                <Text style={styles.userValue}>{user.bairro}</Text>
+                            </View>
+                            <View style={styles.rigthData}>
+                                <Text style={styles.userProperty}>Rua: </Text>
+                                <Text style={styles.userValue}>{user.rua}</Text>
+                            </View>
+                        </View>
+
+                        <View style={styles.address}>
+                            <View>
+                                <Text style={styles.userProperty}>Número: </Text>
+                                <Text style={styles.userValue}>{user.numero}</Text>
+                            </View>
+                            <View style={{ right: 80 }}>
+                                <Text style={styles.userProperty}>UF: </Text>
+                                <Text style={styles.userValue}>{user.uf}</Text>
+                            </View>
+                        </View>
+
+                    </View>
+
+                    <TouchableOpacity
+                            style={styles.signOutButton}
+                            onPress={signOut}
+                        >
+                        <Icon name="arrow-left" size={20} color="#E02041"/>
+                        <Text style={styles.signOutButtonText}>Sair</Text>
+                    </TouchableOpacity>
+
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
--- doacao-legal/src/pages/profile/style.js
+++ doacao-legal/src/pages/profile/style.js
@@ -4,8 +4,73 @@ import Constants from 'expo-constants';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FFF',
+        // backgroundColor: '#FFF',
         paddingHorizontal: 24,
         paddingTop: Constants.statusBarHeight + 20
-    }
+    },
+
+    header: {
+        // flexDirection: 'row',
+        // justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+
+    title: {
+        fontSize: 30,
+        marginBottom: 16,
+        marginTop: 30,
+        color: '#13131a',
+        fontWeight: 'bold',
+    },
+
+    data: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#FFF',
+        marginBottom: 10,
+    },
+
+    edit: {
+        position: 'absolute',
+        alignSelf: 'flex-end',
+        right: 15,
+        top: 15,
+    },
+
+    userProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold',
+    },
+
+    userValue: {
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 14,
+        color: '#737380',
+    },
+
+    address: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    rigthData: {
+        right: 40,
+    },
+
+    signOutButton: {
+        left: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    signOutButtonText: {
+        left: 20,
+        color: '#E02041',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
 });
